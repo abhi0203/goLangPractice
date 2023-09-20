7160e43dc535c21d2677797c923157aabcd96a06
Abhiram <abhiram.vjti@gmail.com>
2023-09-19 23:47:53 -0400

    Hello World and Raw string literal

---
+++ 01GettingWithGo/main.go
@@ -0,0 +1,14 @@
+// Exercise 1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("		This is Abhiram with emoji 🙂")
+
+	fmt.Println(`
+				This is raw string literal.
+				It will print whatever it is as is. So it will not worry about special characters like it will for normal ""
+				`)
+}
